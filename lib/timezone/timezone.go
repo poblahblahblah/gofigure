c7c89a2b6f9fdfaf1cda562481bf7f8bb6e65e88
Jason Heiss <jheiss@aput.net>
2014-09-02 12:42:49 -0400

    Ignore current environment when running date

--- lib/timezone/timezone.go
+++ lib/timezone/timezone.go
@@ -10,6 +10,9 @@ func Load() string {
   arg0 := "+%Z"
 
   cmd := exec.Command(app, arg0)
+  // Ignore the current environment so that we are not influenced by a TZ variable
+  // and instead get the system timezone
+  cmd.Env = []string{}
   out, err := cmd.Output()
 
   if err != nil {
